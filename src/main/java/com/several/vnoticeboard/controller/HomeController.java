9622e9dfde137c79ee6ae2cc97a0bfcb31ae2b29
Murshalin Ridoy <murshalin98@gmail.com>
2022-01-19 20:15:51 +0600

    thymleaf ready

--- src/main/java/com/several/vnoticeboard/controller/HomeController.java
+++ src/main/java/com/several/vnoticeboard/controller/HomeController.java
@@ -1,11 +1,19 @@
 package com.several.vnoticeboard.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 @Controller
 public class HomeController {
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showHome() {
